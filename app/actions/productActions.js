2d31ec082d83cdb67f2afbbe3bf9b55acdea40d0
Link2004tw <fa.csf2023@gmail.con>
2025-10-24 22:12:07 +0300

    add productActions

--- app/actions/productActions.js
+++ app/actions/productActions.js
@@ -3,10 +3,19 @@
 import { createClient } from "@/lib/server";
 import { supabaseAdmin } from "@/lib/supabaseAdmin";
 
+// create a function to verify the user
+async function verifyUser() {}
 export async function addProduct(formData) {
   const supabase = await createClient();
+  //check how to authorize the user
+  // check the validity of the product item
+  // insert the product into the database
 }
 
-export async function deleteProduct(pid) {}
+export async function deleteProduct(pid) {
+  //
+}
 
-export async function updateProduct(formData) {}
+export async function updateProduct(formData) {
+  //
+}
